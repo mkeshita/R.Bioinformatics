3054102279e71c7989fc262cafce88b082985d40
謝桂綱 <xie.guigang@gmail.com>
2016-03-04 19:50:07 +0800

    installer UI

--- Bioconductor/Bioconductor/Web/Installer/InstallPackage.Designer.vb
+++ Bioconductor/Bioconductor/Web/Installer/InstallPackage.Designer.vb
@@ -26,23 +26,32 @@ Partial Class InstallPackage
         Me.TreeView1 = New System.Windows.Forms.TreeView()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
         '
         'TreeView1
         '
+        Me.TreeView1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.TreeView1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TreeView1.Location = New System.Drawing.Point(14, 15)
+        Me.TreeView1.Location = New System.Drawing.Point(12, 13)
         Me.TreeView1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TreeView1.Name = "TreeView1"
-        Me.TreeView1.Size = New System.Drawing.Size(326, 498)
+        Me.TreeView1.Size = New System.Drawing.Size(311, 723)
         Me.TreeView1.TabIndex = 0
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(452, 30)
+        Me.TextBox1.Location = New System.Drawing.Point(436, 27)
         Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(244, 22)
+        Me.TextBox1.Size = New System.Drawing.Size(279, 22)
         Me.TextBox1.TabIndex = 1
         '
         'Label1
@@ -50,18 +59,70 @@ Partial Class InstallPackage
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(362, 30)
+        Me.Label1.Location = New System.Drawing.Point(359, 30)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(54, 16)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Searches"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(725, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "GO!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(339, 82)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(461, 654)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.LinkLabel1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(453, 625)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "bioc Info"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(430, 625)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Search Result"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(313, 589)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(84, 16)
+        Me.LinkLabel1.TabIndex = 0
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Click to install!"
         '
         'InstallPackage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(812, 658)
+        Me.ClientSize = New System.Drawing.Size(820, 749)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.TreeView1)
@@ -70,6 +131,9 @@ Partial Class InstallPackage
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "InstallPackage"
         Me.Text = "BiocViews"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -78,4 +142,9 @@ Partial Class InstallPackage
     Friend WithEvents TreeView1 As Windows.Forms.TreeView
     Friend WithEvents TextBox1 As Windows.Forms.TextBox
     Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Button1 As Windows.Forms.Button
+    Friend WithEvents TabControl1 As Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As Windows.Forms.TabPage
+    Friend WithEvents LinkLabel1 As Windows.Forms.LinkLabel
 End Class
