3650391bfd71ecd0d37f60af7d781f60b4669e20
謝桂綱 <xie.guigang@gmail.com>
2016-03-04 22:32:05 +0800

    config release profiles

--- Bioconductor/Bioconductor/Program.vb
+++ Bioconductor/Bioconductor/Program.vb
@@ -9,19 +9,7 @@ Imports SMRUCC.R.CRAN.Bioconductor.Web.Packages
 Module Program
 
     Public Sub Main()
-
-        Dim ppp = Repository.LoadDefault
-        Dim ddd = ppp.softwares.First.GetDetails
-
-
         Call RSystem.InitDefault()
-
-        Dim nn = RSystem.packageVersion("snow")
-        nn = RSystem.packageVersion("snowx")
-
         Call New InstallPackage(Repository.LoadDefault).ShowDialog()
-
-        Dim bc As WebService = WebService.Default
-        Console.WriteLine(bc.Version)
     End Sub
 End Module
