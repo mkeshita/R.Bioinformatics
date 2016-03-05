8f57858fa5b9a7fb30fa0d7d0194c8b0559bbf87
謝桂綱 <xie.guigang@gmail.com>
2016-03-05 15:20:56 +0800

    gets function name

--- RDotNET.Extensions.VisualBasic/Services/ScriptBuilder/BuilderAPI.vb
+++ RDotNET.Extensions.VisualBasic/Services/ScriptBuilder/BuilderAPI.vb
@@ -1,6 +1,17 @@
 ﻿Public Module BuilderAPI
 
+    Const IsNotAFunc = "Target object is not a R function abstract!"
+
     Public Function GetScript(Of T)(token As T) As String
         Dim type As Type = GetType(T)
+        Dim name As RFunc = type.GetAttribute(Of RFunc)
+
+        If name Is Nothing Then
+            Dim ex As New Exception(IsNotAFunc)
+            ex = New Exception(type.FullName, ex)
+            Throw ex
+        End If
+
+        Dim props = 
     End Function
 End Module
