5ff3b411103f578da5daa3a89ff7fcbedf9ba283
謝桂綱 <xie.guigang@gmail.com>
2016-03-05 15:11:21 +0800

    serialization attributes

--- RDotNET.Extensions.VisualBasic/Services/Helpers/Parameter.vb
+++ RDotNET.Extensions.VisualBasic/Services/Helpers/Parameter.vb
@@ -1,7 +1,42 @@
-﻿Imports Microsoft.VisualBasic.DocumentFormat.Csv.StorageProvider.Reflection
+﻿Imports System.Data.Linq.Mapping
+Imports System.Linq
+Imports Microsoft.VisualBasic.Serialization
 
-Public Class Parameter : Inherits ColumnAttribute
-    Sub New(name As String)
-        Call MyBase.New(name)
+<AttributeUsage(AttributeTargets.Property, AllowMultiple:=False, Inherited:=True)>
+Public Class Parameter : Inherits Attribute
+
+    Public ReadOnly Property [Optional] As Boolean
+    Public ReadOnly Property Name As String
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="name"></param>
+    ''' <param name="opt">Is this parameter optional?</param>
+    Sub New(name As String, Optional opt As Boolean = False)
+        Me.Name = name
+        Me.[Optional] = opt
     End Sub
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
+
+<AttributeUsage(AttributeTargets.Class Or AttributeTargets.Struct, AllowMultiple:=False, Inherited:=True)>
+Public Class RFunc : Inherits Attribute
+
+    Public ReadOnly Property Name As String
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="name">R function name</param>
+    Sub New(name As String)
+        Me.Name = name
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return Name
+    End Function
 End Class
---
+++ RDotNET.Extensions.VisualBasic/Services/ScriptBuilder/BuilderAPI.vb
@@ -0,0 +1,6 @@
+﻿Public Module BuilderAPI
+
+    Public Function GetScript(Of T)(token As T) As String
+        Dim type As Type = GetType(T)
+    End Function
+End Module
