15b11ec624e741d40472d525bf7e61519a2f7cfb
xieguigang <xie.guigang@gmail.com>
2016-03-11 20:18:31 +0800

    add a options api

---
+++ Bioconductor/Bioconductor/biocPkgs/WGCNA/App/WGCNA.vb
@@ -0,0 +1,12 @@
+﻿
+Imports RDotNET.Extensions.VisualBasic
+
+Namespace WGCNA.App
+
+    Public MustInherit Class WGCNA : Inherits IRScript
+
+        Sub New()
+            Requires = {"WGCNA"}
+        End Sub
+    End Class
+End Namespace
