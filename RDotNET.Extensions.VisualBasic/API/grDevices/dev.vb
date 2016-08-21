cdebf69b818c8585c57cecad0ef7cf8d1ed05e64
xieguigang <xie.guigang@gmail.com>
2016-08-21 10:48:41 +0800

    images api

---
+++ RDotNET.Extensions.VisualBasic/API/grDevices/dev.vb
@@ -0,0 +1,14 @@
+﻿Namespace API.grDevices
+
+    Public Module dev
+
+        ''' <summary>
+        ''' ``dev.off`` returns the number and name of the new active device (after the specified device has been shut down).
+        ''' </summary>
+        ''' <param name="which"></param>
+        ''' <returns></returns>
+        Public Function off(Optional which As String = "dev.cur()") As Integer
+            Call $"dev.off(which={which})".ζ
+        End Function
+    End Module
+End Namespace
---
+++ RDotNET.Extensions.VisualBasic/API/grDevices/images.vb
@@ -0,0 +1,37 @@
+﻿
+Imports RDotNET.Extensions.VisualBasic.grDevices
+
+Namespace API.grDevices
+
+    Public Module images
+
+        Public Sub tiff(Optional filename As String = "Rplot%03d.tif",
+                        Optional width As Integer = 480,
+                        Optional height As Integer = 480,
+                        Optional units As String = "px",
+                        Optional pointsize As Integer = 12,
+                        Optional compression As String = "c(""none"", ""rle"", ""lzw"", ""jpeg"", ""zip"", ""lzw+p"", ""zip+p"")",
+                        Optional bg As String = "white",
+                        Optional res As String = "NA",
+                        Optional family As String = "",
+                        Optional restoreConsole As Boolean = True,
+                        Optional type As String = "c(""windows"", ""cairo"")",
+                        Optional antialias As String = NULL)
+
+            Call New tiff With {
+                .antialias = antialias,
+                .filename = filename,
+                .restoreConsole = restoreConsole,
+                .family = family,
+                .compression = compression,
+                .bg = bg,
+                .height = height,
+                .pointsize = pointsize,
+                .res = res,
+                .type = type,
+                .units = units,
+                .width = width
+            }.__call
+        End Sub
+    End Module
+End Namespace
