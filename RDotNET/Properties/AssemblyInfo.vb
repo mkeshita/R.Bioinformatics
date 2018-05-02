e716a0d4bc7c4a910e25b969f6e2e258c1eaa602
谢桂纲 <xie.guigang@gcmodeller.org>
2018-05-02 20:13:56 +0800

    config project properties

--- RDotNET/Properties/AssemblyInfo.vb
+++ RDotNET/Properties/AssemblyInfo.vb
@@ -9,10 +9,10 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("R.NET")>
 <Assembly: AssemblyDescription(".NET bindings for the official R statistical computing language")>
 <Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyCompany("https://github.com/jmp75/rdotnet")>
 <Assembly: AssemblyProduct("R.NET")>
 <Assembly: AssemblyCopyright("Copyright © Copyright 2011-2014 RecycleBin, Copyright © 2014-2017 CSIRO")>
-<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyTrademark("R")>
 <Assembly: AssemblyCulture("")>
 
 ' Setting ComVisible to false makes the types in this assembly not visible
@@ -25,3 +25,5 @@ Imports System.Runtime.InteropServices
 
 <Assembly: Guid("9ecd1b81-67cd-417a-8b4d-77847c3353d4")>
 <Assembly: InternalsVisibleTo("RDotNet.Tests")>
+<Assembly: AssemblyVersion("3.5.0.3256")>
+<Assembly: AssemblyFileVersion("3.4.4.441")>
